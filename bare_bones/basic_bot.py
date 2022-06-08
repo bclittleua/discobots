b6e3162dd227f09b7dbd532501bbe19de2b10fff
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-08 11:13:36 -0700

    Update basic_bot.py

--- bare_bones/basic_bot.py
+++ bare_bones/basic_bot.py
@@ -12,35 +12,16 @@ from discord.ext import commands
 ###########################################################################
 client = commands.Bot(command_prefix=".",intents=discord.Intents.all())
 ###########################################################################
-#Command Format #1, using context(ctx)
 @client.event
 async def on_ready():
     print(f"logged in as {client.user}\n\n")
     chanID = Your-channel_ID-here-no-quotes
     channel = client.get_channel(chanID)
     await channel.send("Discord Bot Online.") #NOTE: this sends to a specified channel, chanID
-
-#Conversely, the context (ctx) data captures which user sent what and from where
-#without having to track all those ids:
-@client.command()
-async def hello(ctx):
-    await ctx.channel.send(f"Hello! {ctx.author.mention}") 
-    #ctx.author is the user that sent the command and replies with @username (.mention)
-
-@client.command()
-async def ping(ctx): #built-in funtion to get your ping, neat
-    await ctx.channel.send(f"Pong {round(client.latency*1000)} ms")
-    
-@client.command()
-async def quote(ctx): #this one just reads and returns a file
-      await ctx.channel.send(open('/path/to/desired/file.txt').read())
-      
-      #WARNING: the file path above is a linux format! for WINDOWS use the following:
-      await ctx.channel.send(open('C:\Users\YOUR!-USERNAME\Desktop\another_folder\file.txt').read())
 ###########################################################################
-#Commmand Format #2, commands as a response to @client.event
-#aka, when the bot detects a match in chat it fires commands as a response
-#think: IF this THEN that AND that_other
+# Commands as a response to @client.event
+# aka, when the bot detects a match in chat it fires commands as a response
+# can chain these, think: IF this THEN that AND that_other
 @client.event
 async def on_message(message):
     if message.content.startswith("/custom_query"):
